513409276e10f1519f110852a60e23f55ebe25f5
Ritikesh-Raj <ritikesh.raj04@gmail.com>
2023-07-18 18:16:17 +0530

    progressing

---
+++ src/components/About.jsx
@@ -0,0 +1,46 @@
+import React from 'react'
+
+const About = () => {
+  return(  
+    <div name="about" className="w-full h-screen bg-gradient-to-b
+    from-gray-800 to-black text-white"
+    >
+
+      <div className="max-w-screen-lg p-4 mx-auto 
+      flex flex-col justify-center w-full h-full">
+        <div className="pb-8">
+            <p className="text-4xl font-bold border-gray-500
+            inline border-b-4">
+            About
+            </p>
+        </div>
+
+        <p className="text-lg mt-20">
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+            Vero perspiciatis nobis modi recusandae voluptatum eaque, 
+            nisi harum minima numquam delectus dolor pariatur, tempore 
+            ea necessitatibus placeat veniam expedita suscipit iusto. 
+            Id, suscipit eos. Perspiciatis rerum, cumque placeat porro 
+            eius tenetur ea debitis? Aperiam ut, porro cupiditate nihil 
+            atque asperiores. Natus!
+        </p>
+
+        <br />
+
+        <p className="text-xl">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+            Assumenda nam doloribus esse accusamus veritatis? Asperiores 
+            laborum, mollitia eius laboriosam pariatur dignissimos id 
+            architecto aut quisquam eligendi corporis rerum, maxime aspernatur, 
+            nisi qui voluptatem! Unde pariatur consectetur sapiente dolores. 
+            Laborum impedit placeat similique iure temporibus labore eveniet 
+            ratione odit consequatur eligendi.
+        </p>
+      </div>
+
+
+    </div>
+  )
+}
+
+export default About
