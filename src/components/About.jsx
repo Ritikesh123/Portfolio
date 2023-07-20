1f8c66c379d797825b63e23a7522abc787c297a1
Ritikesh-Raj <ritikesh.raj04@gmail.com>
2023-07-20 11:43:24 +0530

    about

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -8,34 +8,41 @@ const About = () => {
 
       <div className="max-w-screen-lg p-4 mx-auto 
       flex flex-col justify-center w-full h-full">
-        <div className="pb-8">
+        <div className="pb-20">
             <p className="text-4xl font-bold border-gray-500
             inline border-b-4">
             About
             </p>
         </div>
 
-        <p className="text-lg mt-20">
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-            Vero perspiciatis nobis modi recusandae voluptatum eaque, 
-            nisi harum minima numquam delectus dolor pariatur, tempore 
-            ea necessitatibus placeat veniam expedita suscipit iusto. 
-            Id, suscipit eos. Perspiciatis rerum, cumque placeat porro 
-            eius tenetur ea debitis? Aperiam ut, porro cupiditate nihil 
-            atque asperiores. Natus!
+        <p className="text-lg mt-10">
+        Hi...... I'm Ritikesh Raj as a Frontend Developer with 1.5+ years of experience, 
+        I am driven by a deep passion for crafting exceptional user experiences and 
+        bringing creative designs to life through code.
         </p>
-
         <br />
 
-        <p className="text-xl">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-            Assumenda nam doloribus esse accusamus veritatis? Asperiores 
-            laborum, mollitia eius laboriosam pariatur dignissimos id 
-            architecto aut quisquam eligendi corporis rerum, maxime aspernatur, 
-            nisi qui voluptatem! Unde pariatur consectetur sapiente dolores. 
-            Laborum impedit placeat similique iure temporibus labore eveniet 
-            ratione odit consequatur eligendi.
+
+        <p className="text-lg">
+        I hold a Bachelor of Engineering in Electrical and Electronics Engineering from 
+        BIT Mesra Ranchi, where I honed my technical skills and gained a solid foundation
+        in web development principles.
         </p>
+        <br />
+
+        
+        <p className="text-lg">
+        Throughout my career, I have worked on a diverse range of projects, collaborating 
+        with cross-functional teams to build responsive and user-friendly websites and web 
+        applications. My expertise lies in HTML, CSS, and JavaScript, and I am well-versed 
+        in utilizing modern frameworks such as React and Vue.js to deliver seamless frontend 
+        solutions. Some of my own driven project you can see in my Portfolio section.
+        </p>
+        <br />
+
+
+
+
       </div>
 
 
