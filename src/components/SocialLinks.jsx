d99372c2423ef5ec9bdcd9a0c5df3a623194d8ea
Ritikesh-Raj <ritikesh.raj04@gmail.com>
2023-07-22 00:01:02 +0530

    cv-change

--- src/components/SocialLinks.jsx
+++ src/components/SocialLinks.jsx
@@ -41,7 +41,7 @@ const SocialLinks = () => {
                 Resume <BsFillPersonLinesFill size={30} />
                 </>
             ),
-            href:"/resume.pdf",
+            href:"/resume-ritikesh.pdf",
             style:"rounded-br-md",
             download: true,
             
