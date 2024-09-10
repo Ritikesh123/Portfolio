240c3b2638a3e8d13adc9aef68670c16c52fb47e
Ritikesh-Raj <ritikesh@thor.solutions>
2024-09-11 01:05:15 +0530

    update on About

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -16,27 +16,33 @@ const About = () => {
         </div>
 
         <p className="text-lg mt-10">
-        Hi...... I'm Ritikesh Raj as a Frontend Developer almost 3 years of experience, 
-        I am driven by a deep passion for crafting exceptional user experiences and 
-        bringing creative designs to life through code.
+          I am Ritikesh Raj, a Frontend Developer with nearly 3 years of experience, specializing in creating dynamic, 
+          user-centric web solutions. My journey has been driven by a passion for designing exceptional user experiences
+          and translating creative concepts into high-performance code.
         </p>
         <br />
 
 
         <p className="text-lg">
-        I hold a Bachelor of Engineering in Electrical and Electronics Engineering from 
-        BIT Mesra Ranchi, where I honed my technical skills and gained a solid foundation
-        in web development principles.
+          I hold a Bachelor of Engineering in Electrical and Electronics Engineering from BIT Mesra Ranchi, 
+          where I built a strong technical foundation. This has enabled me to approach web development with 
+          a problem-solving mindset, driving impactful solutions in my work.
+
         </p>
         <br />
 
         
         <p className="text-lg">
-        Throughout my career, I have worked on a diverse range of projects, collaborating 
-        with cross-functional teams to build responsive and user-friendly websites and web 
-        applications. My expertise lies in HTML, CSS, and JavaScript, and I am well-versed 
-        in utilizing modern frameworks such as React and Vue.js to deliver seamless frontend 
-        solutions. Some of my own driven project you can see in my Portfolio section.
+          Throughout my career, I’ve had the privilege of working with companies like Sapphire Human Solution 
+          and Tech Mahindra, where I contributed to building responsive, scalable, and efficient web applications. 
+          I collaborated with cross-functional teams to deliver new features, optimize performance, and ensure seamless 
+          frontend-backend integration. Proficient in HTML, CSS, and JavaScript, I am well-versed in frameworks such as 
+          ReactJS, which I use to deliver top-tier frontend solutions.
+        </p>
+        <br />
+
+        <p className="text-lg">
+          My portfolio highlights several self-driven projects, reflecting my dedication to creating innovative and user-friendly web applications.
         </p>
         <br />
 
